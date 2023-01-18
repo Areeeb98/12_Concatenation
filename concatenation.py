6722931327b64c79336dd2a57b60c159a87f7368
Areeeb98 <122514572+Areeeb98@users.noreply.github.com>
2023-01-18 19:54:35 +0530

    Add files via upload

---
+++ concatenation.py
@@ -0,0 +1,19 @@
+"""
+Day: Wednesday
+Date: 18th Jan 2023
+Time: 6:45 PM
+Topic: Concatination Examples
+Author: Areeb Ahmed
+"""
+
+
+a="Python" ; b="Programming"
+print(a+b)
+print(a,b)
+print(a+' '+b)
+print(a+chr(32)+b)
+
+a="5" ; b="7" ; c=8
+print(a+b)
+print(a,b+str(c))
+print(a+" "+b+str(c))
